7a2b2ea3692e31b254c8eb680dd51d696355fa59
ubska <alessandraubska@gmail.com>
2024-09-12 16:07:53 +0200

    feat: add $table info

--- database/migrations/2024_09_12_135956_create_trains_table.php
+++ database/migrations/2024_09_12_135956_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda');
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno');
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
             $table->timestamps();
         });
     }
