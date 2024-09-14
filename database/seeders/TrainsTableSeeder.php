a8c61dee400db575fe486f6a06c090042d9dd9e9
ubska <alessandraubska@gmail.com>
2024-09-14 16:51:42 +0200

    create seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        foreach (range(1, 50) as $index) {
+            DB::table('trains')->insert([
+                'azienda' => $faker->company,
+                'stazione_partenza' => $faker->city,
+                'stazione_arrivo' => $faker->city,
+                'orario_partenza' => $faker->time('H:i:s'),
+                'orario_arrivo' => $faker->time('H:i:s'),
+                'codice_treno' => $faker->unique()->word,
+                'numero_carrozze' => $faker->numberBetween(5, 20),
+                'in_orario' => $faker->boolean,
+                'cancellato' => $faker->boolean,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
